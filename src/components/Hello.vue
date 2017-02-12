0495668925eff7ef3c9950cce8b1db606768ae92
Matias Tucci <matiastucci@gmail.com>
2017-02-12 16:08:05 -0300

    add animation

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="animated fadeIn">{{ msg }}</h1>
   </div>
 </template>
 
@@ -16,8 +16,42 @@ export default {
 </script>
 
 <style scoped>
+  @charset "UTF-8";
+
   h1 {
     font-weight: normal;
     margin: 0;
+  }
+
+  .animated {
+    -webkit-animation-duration: 1s;
+    animation-duration: 1s;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+  }
+
+  @-webkit-keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+
+    to {
+      opacity: 1;
+    }
+  }
+
+  .fadeIn {
+    -webkit-animation-name: fadeIn;
+    animation-name: fadeIn;
   }
 </style>
