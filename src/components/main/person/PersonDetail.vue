0b09337e614a2cb57f21da083f58a1eb89b6d5cd
Matias Tucci <matiastucci@gmail.com>
2017-03-30 02:23:26 -0400

    person detail

---
+++ src/components/main/person/PersonDetail.vue
@@ -0,0 +1,24 @@
+<script>
+  import personService from 'services/person';
+
+  export default {
+    name: 'person',
+
+    data() {
+      return {
+        person: {},
+      };
+    },
+
+    mounted() {
+      personService.getById(this.$route.params.personId).then((person) => {
+        this.person = person;
+      });
+    },
+  };
+</script>
+
+<template lang="pug">
+  section#person
+    pre {{ person }}
+</template>
