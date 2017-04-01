61f7a6abd71be806a4005daa5d93fba9955f804a
Matias Tucci <matiastucci@gmail.com>
2017-04-01 04:31:12 -0400

    export map styles

--- src/services/config/index.js
+++ src/services/config/index.js
@@ -1,8 +1,11 @@
+import mapStyles from 'services/map-styles';
+
 const config = {};
 
 config.googleMapsKey = 'AIzaSyBFDFmn-PL1Kg0frwZUXibuFyuiTPDMsas';
 
 config.map = {
+  styles: mapStyles,
   icon: {
     male: '/static/person-male.svg',
     female: '/static/person-female.svg',
