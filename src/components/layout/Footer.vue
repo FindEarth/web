92021c78a7b3c4cfe26e95b2398eaedd2a8d44f6
Matias Tucci <matiastucci@gmail.com>
2017-04-01 04:30:35 -0400

    footer

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  section#footer
+    //- a.link.hover-effect(href='https://keepe.rs', target='_blank') keepers
+</template>
+
+<style lang="scss">
+  section#footer {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+  }
+</style>
