cbbde1947c38627e4719f781a99cd8e1186fc800
Matias Tucci <matiastucci@gmail.com>
2017-04-09 18:52:30 -0400

    remove footer from default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,4 @@
-<script>
-  import MyFooter from '~components/Footer.vue'
-
-  export default {
-    components: {
-      MyFooter
-    }
-  }
-</script>
-
 <template lang="pug">
   .content
     nuxt
-    my-footer
 </template>
-
-<style lang="scss" scoped>
-  .content {
-    margin-top: 35px;
-  }
-</style>
