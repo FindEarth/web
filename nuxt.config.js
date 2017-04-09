70e5110ca0d00a26b09a3671b2d0f70072ce2f24
Matias Tucci <matiastucci@gmail.com>
2017-04-09 00:24:56 -0400

    update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,10 +7,10 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Find Earth: open source platform to help tracing missing persons' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
   /*
