0584ab2bdd5952a1fd8c2d5bf127a29bbb9d688b
ndelvalle <nicolas.delvalle@gmail.com>
2017-04-09 16:56:43 -0400

    Add config, trae and maps plugins

---
+++ plugins/config.js
@@ -0,0 +1,14 @@
+import mapStyle from './map-style'
+
+const config = {
+  map: {
+    key: 'AIzaSyBFDFmn-PL1Kg0frwZUXibuFyuiTPDMsas',
+    style: mapStyle,
+    icons: {
+      male: '/static/person-male.svg',
+      female: '/static/person-female.svg'
+    }
+  }
+}
+
+export default config
