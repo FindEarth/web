78e8b369c23dc7e63cc53443a907191d180985a9
ndelvalle <nicolas.delvalle@gmail.com>
2017-04-10 01:29:16 -0400

    Create footer using skeleton

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,12 +1,12 @@
 <template lang="pug" scoped>
-  .container.asd
-    .row
-      .one-half.column
-        a.keepers-logo.hover-effect(href='https://keepe.rs', target='_blank')
-          img(src='/keepers-logo.png')
-      //- .five.column
-      //-   asd
-      .one-half.column
+  .row
+    .one.column
+      a.keepers-logo.hover-effect(href='https://keepe.rs', target='_blank')
+        img(src='/keepers-logo.png')
+    .eight.columns
+      span &nbsp;
+    .three.columns
+      .social-icons
         a.hover-effect(href='https://facebook.com/FindEarth', target='_blank')
           i.fa.fa-facebook
         a.hover-effect(href='https://twitter.com/FindEarthOrg', target='_blank')
@@ -15,24 +15,22 @@
           i.fa.fa-github
 </template>
 
-<style lang="scss">
-  .container {
-    .row {
-      .keepers-logo img {
-        width: 25px;
+<style lang="scss" scoped>
+  .row {
+    .keepers-logo img {
+      width: 20px;
+    }
+
+    .social-icons {
+      text-align: right;
+      font-size: 1em;
+
+      a {
+        color: #29235C;
       }
 
-      .social-icons {
-        text-align: right;
-        font-size: 1.2em;
-
-        a {
-          color: #29235C;
-        }
-
-        i.fa {
-          width: 50px;
-        }
+      i.fa {
+        width: 40px;
       }
     }
   }
