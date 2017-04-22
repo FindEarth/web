590d6e66951a1353ffef037b530047410a8eb1fd
Matias Tucci <matiastucci@gmail.com>
2017-04-22 04:58:47 -0400

    support multiple aliases in the env variable

--- scripts/set-now-alias.js
+++ scripts/set-now-alias.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
 const nowConfig = require('../now.json')
-nowConfig.alias = process.env.ALIAS || 'find.earth'
+let alias = process.env.ALIAS || 'find.earth'
+nowConfig.alias = alias.replace(/ /g, '').split(',')
 fs.writeFileSync('now.json', JSON.stringify(nowConfig, null, 2), 'utf8')
