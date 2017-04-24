b1d5a168f2cda627b075f4d1fae69c9876c14d10
Matias Tucci <matiastucci@gmail.com>
2017-04-24 05:06:21 -0400

    add transition on hover

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -6,13 +6,13 @@
           img(src='/keepers-logo.png')
       .col-xs-6.col-sm-offset-9.col-sm-1
         .social-icons
-          a(href='https://opencollective.com/findearth', target='_blank')
+          a.opacity-hover(href='https://opencollective.com/findearth', target='_blank')
             i.fa.fa-handshake-o
-          a(href='https://facebook.com/FindEarth', target='_blank')
+          a.opacity-hover(href='https://facebook.com/FindEarth', target='_blank')
             i.fa.fa-facebook
-          a(href='https://twitter.com/FindEarthOrg', target='_blank')
+          a.opacity-hover(href='https://twitter.com/FindEarthOrg', target='_blank')
             i.fa.fa-twitter
-          a(href='https://github.com/FindEarth', target='_blank')
+          a.opacity-hover(href='https://github.com/FindEarth', target='_blank')
             i.fa.fa-github
 </template>
 
@@ -20,19 +20,30 @@
   footer {
     margin-bottom: 10px;
 
+    // TODO: move transition on hover to a class in main.css
+
     .keepers-logo {
       img {
         width: 18px;
+        transition: opacity 0.3s;
+        &:hover {
+          cursor: pointer;
+          opacity: 0.7;
+        }
       }
     }
 
     .social-icons {
       display: flex;
       justify-content: flex-end;
-
       a {
         padding-left: 15px;
         color: #29235C;
+        transition: opacity 0.3s;
+        &:hover {
+          cursor: pointer;
+          opacity: 0.7;
+        }
       }
     }
   }
