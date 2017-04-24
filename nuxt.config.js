47cf43a28e27bc2220b6ac336e90e1f563613483
Mati Tucci <matiastucci@gmail.com>
2017-04-23 21:29:52 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   head: {
-    title: 'FindEarth 🔎🌎',
+    title: 'Find Earth',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
