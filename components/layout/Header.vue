deb81350a659ab441e736c63f208b3d3e248dd41
Matias Tucci <matiastucci@gmail.com>
2017-06-23 03:50:03 +0200

    new header

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,13 +1,6 @@
 <script>
   export default {
-    name: 'Header',
-
-    props: {
-      title: {
-        type: String,
-        required: false
-      }
-    }
+    name: 'Header'
   }
 </script>
 
@@ -15,57 +8,52 @@
   .header
     .logo
       nuxt-link(to="/")
-        img.animated-logo(src="/animated-logo.svg")
-    .title(v-if="title") {{ title }}
-    .social-icons
-      a.opacity-hover(href="https://opencollective.com/findearth", target="_blank")
-        i.fa.fa-handshake-o
-      a.opacity-hover(href="https://facebook.com/FindEarth", target="_blank")
-        i.fa.fa-facebook
-      a.opacity-hover(href="https://twitter.com/FindEarthOrg", target="_blank")
-        i.fa.fa-twitter
-      a.opacity-hover(href="https://github.com/FindEarth", target="_blank")
-        i.fa.fa-github
+        img.animated-logo(src="/animated-white-logo.svg")
+    ul.items
+      li.item
+        nuxt-link(to="/") Inicio
+      li.item
+        nuxt-link(to="/") Nosotros
+      li.item
+        nuxt-link(to="/") Personas
+      li.item
+        nuxt-link(to="/") Sumarse
 </template>
 
 <style lang="scss" scoped>
   .header {
     $logo-width: 40px;
 
+    display: -ms-flexbox;
     display: flex;
+    -ms-flex-align: center;
     align-items: center;
-    height: 64px;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    background: rgba(22, 22, 22, 0.01);
-    box-shadow: 0 0 2px 0px rgba(22, 22, 22, 0.2);
-    border-bottom: 1px solid #DBDBDB;
-    padding: 0 15px;
+    height: 70px;
+    background: #29235C;
+    color: #fff;
+    padding: 0 80px;
 
     .logo {
       width: $logo-width;
       justify-content: flex-start;
     }
 
-    .title {
-      flex: 1;
-      text-align: center;
-      font-family: 'Avenir', Helvetica, Arial, sans-serif;
-      font-weight: bold;
-      font-size: 1.5em;
-      margin-left: 20px;
-    }
-
-    .social-icons {
+    ul.items {
+      list-style: none;
       margin-left: auto;
-      .fa {
-        text-align: center;
-        width: 25px;
-        transition: opacity 0.3s;
-        &:hover {
-          opacity: 0.7;
+
+      li.item {
+        display: inline;
+        margin: 0 10px;
+
+        a {
+          color: #fff;
+          text-decoration: none;
+          transition: opacity .3s;
+
+          &:hover {
+            opacity: .6;
+          }
         }
       }
     }
