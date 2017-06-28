370c9d3b17727a01fd918c0f2327eb44da6bc5fe
delvallejonatan <delvallejonatan@gmail.com>
2017-06-28 03:58:40 +0200

    Add responsive styles

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -5,7 +5,18 @@
   export default {
     name: 'Header',
 
-    components: { HamburgerButton }
+    components: { HamburgerButton },
+
+    methods: {
+      toogleMenuClass () {
+        this.$el.classList.toggle('open')
+      },
+
+      toogleMenuClassItem () {
+        this.$el.classList.toggle('open')
+        this.$el.querySelector('.hamburger').classList.toggle('is-active')
+      }
+    }
   }
 </script>
 
@@ -15,16 +26,16 @@
       nuxt-link(to="/")
         img.animated-logo(src="/animated-white-logo.svg")
     ul.items
-      li.item
+      li.item(@click="toogleMenuClassItem")
         nuxt-link(to="/") Inicio
-      li.item
+      li.item(@click="toogleMenuClassItem")
         nuxt-link(to="/about") Acerca
-      li.item
+      li.item(@click="toogleMenuClassItem")
         nuxt-link(to="/persons") Personas
-      li.item
+      li.item(@click="toogleMenuClassItem")
         nuxt-link(to="/sumarse") Sumarse
-      li.item.hamburguer-container
-        hamburger-button
+    .hamburguer-container(@click="toogleMenuClass")
+      hamburger-button
 </template>
 
 <style lang="scss" scoped>
@@ -38,18 +49,24 @@
     color: #fff;
     padding: 0 3em;
     z-index: 4;
+    position: relative;
+
+    &.open {
+      ul.items {
+        @media (max-width: 600px) {
+          height: 184px;
+        }
+      }
+    }
 
     .hamburguer-container {
-      display: none !important;
+      display: none;
+      margin-left: auto;
     }
 
     @media (max-width: 600px) {
-
-      li.item {
-        display: none !important;
-        &.hamburguer-container {
-          display: inherit !important;
-        }
+      .hamburguer-container {
+        display: block;
       }
     }
 
@@ -62,15 +79,48 @@
       list-style: none;
       margin-left: auto;
 
+      @media (max-width: 600px) {
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 100%;
+        margin: 0;
+        padding: 0;
+        border-bottom: 1px solid #DAE1E9;
+        height: 0;
+        overflow: hidden;
+        transition-duration: 0.13s;
+        transition-delay: 0.13s;
+        transition-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+      }
+
       li.item {
         display: inline;
         margin-left: 20px;
+
+        @media (max-width: 600px) {
+          display: block;
+          width: 100%;
+          border-top: 1px solid #DAE1E9;
+          margin: 0;
+          background: #f4f7fa;
+          cursor: pointer;
+        }
 
         a {
           color: #fff;
           text-decoration: none;
           transition: opacity .3s;
           cursor: pointer;
+
+          @media (max-width: 600px) {
+            padding: 0 3.5em;
+            height: 45px;
+            display: block;
+            line-height: 45px;
+            position: relative;
+            color: #29235C;
+          }
 
           &:hover {
             opacity: .6;
