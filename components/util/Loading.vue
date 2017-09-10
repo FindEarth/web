38dd4e190051ae50f49a743fc1901aab18fd17cb
Matias Tucci <matiastucci@gmail.com>
2017-09-10 12:45:42 -0300

    use old loading

--- components/util/Loading.vue
+++ components/util/Loading.vue
@@ -5,43 +5,12 @@
 </script>
 
 <template lang="pug">
-  .loading-container
-    img.logo-loading.animated.logo-pulse.infinite(src="/logo.svg")
-  //- .spinner
+  .spinner
     .double-bounce1
     .double-bounce2
 </template>
 
 <style lang="scss">
-  .loading-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 2em;
-  }
-
-  @keyframes logo-pulse {
-    from {
-      transform: scale3d(1, 1, 1);
-    }
-
-    50% {
-      transform: scale3d(1.10, 1.10, 1.10);
-    }
-
-    to {
-      transform: scale3d(1, 1, 1);
-    }
-  }
-
-  .logo-pulse {
-    animation-name: logo-pulse;
-  }
-
-  .logo-loading {
-    width: 100px;
-  }
-
   .spinner {
     width: 40px;
     height: 40px;
